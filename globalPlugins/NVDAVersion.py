9989649f7e368880afec56b86ba97dd6884f69b9
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-03-11 08:39:45 -0700

    We now have help for when pressing the keys in learn keys mode.

--- globalPlugins/NVDAVersion.py
+++ globalPlugins/NVDAVersion.py
@@ -12,10 +12,10 @@ import ui
 import versionInfo
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	@script(gesture="kb:NVDA+shift+v", help="Speak the current NVDA version.")
+	@script(gesture="kb:NVDA+shift+v", description="Speak the current NVDA version.")
 	def script_announceNvdaVersionRegular(self, gesture):
 		ui.message("Version " + versionInfo.version)
 
-	@script(gesture="kb:NVDA+shift+control+v", help="Show the current version of NVDA in a dialog.")
+	@script(gesture="kb:NVDA+shift+control+v", description="Show the current version of NVDA in a dialog.")
 	def script_announceNvdaVersionBrowseable(self, gesture):
 		ui.browseableMessage("Version " + versionInfo.version + "\r\nPress escape to exit.", title="NVDA Version.")
